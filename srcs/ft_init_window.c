a414c7b4063d6f4fa9960497517eff228fb1fae4
lminta <lminta@student.21-school.ru>
2019-11-06 19:46:40 +0300

    ЕБУЧИЕ САБМОДУЛИ

--- srcs/ft_init_window.c
+++ srcs/ft_init_window.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_init_window.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jblack-b <jblack-b@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lminta <lminta@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/18 12:44:17 by jblack-b          #+#    #+#             */
-/*   Updated: 2019/10/31 18:49:40 by jblack-b         ###   ########.fr       */
+/*   Updated: 2019/11/06 19:46:14 by lminta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,13 @@ void	ft_init_window(t_sdl *sdl, size_t win_w, size_t win_h)
 	sdl->win_w = win_w;
 	sdl->surface = SDL_CreateRGBSurface(0, win_w, win_h, 32,\
 	RMASK, GMASK, BMASK, AMASK);
-	SDL_CreateWindowAndRenderer(win_w,\
-		win_h, 0, &(sdl->window), &sdl->renderer);
+	sdl->window = SDL_CreateWindow("ОГОЛИ ЖОПУ ПОЖИЛОГО",
+	SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+	win_w, win_h, 0);
+	sdl->renderer = SDL_CreateRenderer(sdl->window, -1,
+	SDL_RENDERER_ACCELERATED);
+	// SDL_CreateWindowAndRenderer(win_w,\
+	// 	win_h, 0, &(sdl->window), &sdl->renderer);
 	SDL_ShowCursor(SDL_TRUE);
 	sdl->texture = SDL_CreateTexture(sdl->renderer, SDL_PIXELFORMAT_ARGB8888,
 								SDL_TEXTUREACCESS_STREAMING,
